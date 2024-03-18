0a5d4c04de608b10d5bd39bd4769bd569e11be0c
digvijaybind <binddigvijay123@gmail.com>
2024-03-18 23:54:41 +0400

    chnages in logic

--- configs/cronjob.js
+++ configs/cronjob.js
@@ -1,40 +1,34 @@
-const cron=require('node-cron');
-const qs = require('qs');
+const cron = require("node-cron");
+const qs = require("qs");
 const axios = require("axios");
 const data = {
-  client_id: 'vppVZGQGOnwkvZBSjgysgUzUUIdSAF6u',
-  client_secret: 'uA0UEOCy3vIIkWZD',
-  grant_type:'client_credentials'
+  client_id: "vppVZGQGOnwkvZBSjgysgUzUUIdSAF6u",
+  client_secret: "uA0UEOCy3vIIkWZD",
+  grant_type: "client_credentials",
 };
-let access_token = "XasPU2nrY1obUsDRmZogiX2uRorw";
+let access_token = "xCAaKgRtok6sadjNMfdCqtpkyGuE";
 const urlEncodedData = qs.stringify(data);
-const apiUrl = 'https://test.api.amadeus.com/v1/security/oauth2/token';
-
+const apiUrl = "https://test.api.amadeus.com/v1/security/oauth2/token";
 
 async function getProcessedApiData() {
   try {
-    const response =
-    await axios.post(apiUrl, urlEncodedData, {
+    const response = await axios.post(apiUrl, urlEncodedData, {
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
     });
-    
+
     access_token = response.data.access_token;
-    console.log('access_token',response.data)
-    console.log("this access_token",access_token);
-
+    console.log("access_token", response.data);
+    console.log("this access_token", access_token);
   } catch (error) {
-    console.error('Error fetching or processing API data:', error);
-   access_token=null
+    console.error("Error fetching or processing API data:", error);
+    access_token = null;
   }
-
 }
 
-cron.schedule('*/2 * * * *', async () => {
+cron.schedule("*/2 * * * *", async () => {
   const rawApiData = await getProcessedApiData();
 });
-
-
 
 module.exports = {access_token};
