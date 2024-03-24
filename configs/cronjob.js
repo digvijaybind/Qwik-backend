c9c1242410dffaf25e318d6ed870ee1d7ec7f65e
digvijaybind <binddigvijay123@gmail.com>
2024-03-24 23:03:22 +0400

    changes in latest branch

--- configs/cronjob.js
+++ configs/cronjob.js
@@ -6,7 +6,7 @@ const data = {
   client_secret: 'uA0UEOCy3vIIkWZD',
   grant_type: 'client_credentials',
 };
-let access_token = 'GGQnteXzRoqjuulOMsNiMJ2TCAz5';
+let access_token = 'LMzjFHPCebw2Qlji2najnOTm1r4N';
 const urlEncodedData = qs.stringify(data);
 const apiUrl = 'https://test.api.amadeus.com/v1/security/oauth2/token';
 
