8f291a0f976ad38737bad633f3a197a3b0277ea5
digvijaybind <binddigvijay123@gmail.com>
2024-03-26 10:00:11 +0400

    changes in card

--- configs/cronjob.js
+++ configs/cronjob.js
@@ -6,7 +6,7 @@ const data = {
   client_secret: 'uA0UEOCy3vIIkWZD',
   grant_type: 'client_credentials',
 };
-let access_token = 'XqpHZhoLcmE96acVDPSqDLKWSgDV';
+let access_token = 'QalvBDb5J3IbufAdrefGWqSebZo2';
 const urlEncodedData = qs.stringify(data);
 const apiUrl = 'https://test.api.amadeus.com/v1/security/oauth2/token';
 
