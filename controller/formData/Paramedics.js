3230ce1a67e6f2090ecafe78458412044b2a442b
Tijani oluwagbenga miracle <tijanioluwagbengamiracle@gmail.com>
2024-03-26 08:19:38 +0100

    everything is uptodate at deploy branch

--- controller/formData/Paramedics.js
+++ controller/formData/Paramedics.js
@@ -1,7 +1,7 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 const { superBase } = require('../../configs/supabase');
-const multer = require('multer')
-const {Paramedics} = require("../../db/Paramedics");
+const multer = require('multer');
+const { Paramedics } = require('../../db/Paramedics');
 // const generateToken = require("../configs/jwtToken");
 // const OperatorService = require("../services/operator-service");
 
@@ -9,9 +9,7 @@ const storage = multer.memoryStorage(); // Use memory storage for multer
 const upload = multer({ storage: storage }).single('resume_docFilePath');
 
 exports.RegisterParamedics = async (req, res, next) => {
-
   try {
-
     upload(req, res, async function (err) {
       if (err) {
         console.error(err);
@@ -21,9 +19,11 @@ exports.RegisterParamedics = async (req, res, next) => {
       // Check if req.file contains the uploaded file information
       console.log(req.file);
 
-      const {country, location,degrees } = req.body;
+      const { country, location, degrees } = req.body;
       if (!country && !location && !degrees) {
-        return res.status(400).json({ error:'country,location and degrees field are required' });
+        return res
+          .status(400)
+          .json({ error: 'country,location and degrees field are required' });
       }
 
       try {
@@ -32,11 +32,13 @@ exports.RegisterParamedics = async (req, res, next) => {
         const originalFilename = req.file.originalname; // Get the original filename
 
         // Upload the image to Supabase storage with the correct content type and original filename
-        const { data, error } = await superBase.storage.from('Form-data').upload(originalFilename, fileBuffer, {
-          cacheControl: '3600',
-          upsert: false,
-          contentType: fileContentType, // Use the content type from the uploaded file
-        });
+        const { data, error } = await superBase.storage
+          .from('Form-data')
+          .upload(originalFilename, fileBuffer, {
+            cacheControl: '3600',
+            upsert: false,
+            contentType: fileContentType, // Use the content type from the uploaded file
+          });
 
         if (error) {
           console.error(error);
@@ -44,43 +46,36 @@ exports.RegisterParamedics = async (req, res, next) => {
         }
         const fileUrlPath = data.path;
         console.log(fileUrlPath);
-        
 
         if (!fileUrlPath) {
           return res.status(500).json({ error: 'File upload failed' });
         }
-    
-        const fileUrl= await superBase
-         .storage
-         .from('Form-data')
-         .getPublicUrl(fileUrlPath)
-       
-       console.log(fileUrl)
 
-    
-        const  paramedics= new Paramedics({
+        const fileUrl = await superBase.storage
+          .from('profile')
+          .getPublicUrl(fileUrlPath);
+
+        console.log(fileUrl);
+
+        const paramedics = new Paramedics({
           country,
           location,
           degrees,
-          resumeDocumentPath: fileUrl.data.publicUrl
+          resumeDocumentPath: fileUrl.data.publicUrl,
         });
 
-        
         await paramedics.save();
-      console.log("This new Doctor",paramedics)
+        console.log('This new Doctor', paramedics);
         return res.json({
           msg: 'Paramedics created successfully',
-          data:paramedics,
+          data: paramedics,
           success: true,
         });
-
       } catch (err) {
         console.error(err);
         return res.status(500).json({ error: 'Server error' });
       }
     });
-
-    
   } catch (error) {
     throw new Error(error);
   }
