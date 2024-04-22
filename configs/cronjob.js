25cb51e08e8cf83d93d402c93cd725c227fbea4b
digvijaybind <binddigvijay123@gmail.com>
2024-04-22 20:04:47 +0400

    changes in front end

--- configs/cronjob.js
+++ configs/cronjob.js
@@ -6,7 +6,7 @@ const data = {
   client_secret: 'uA0UEOCy3vIIkWZD',
   grant_type: 'client_credentials',
 };
-let access_token = "jAOFeswcsG9dPIVUM57wPfh2wrZD";
+let access_token = 'GdDLpWAcedfwXflGJ40z5LWyy8AU';
 const urlEncodedData = qs.stringify(data);
 const apiUrl = 'https://test.api.amadeus.com/v1/security/oauth2/token';
 
