03ef43c3a63ad43bcac6f040e6fd08b9b61aaf32
Tijani oluwagbenga miracle <tijanioluwagbengamiracle@gmail.com>
2024-04-23 12:10:02 +0100

    working

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieSession = require('cookie-session');
+const axios = require('axios');
 const app = express();
 const OperatorRouter = require('./routes/Operator-Router');
 const CustomerRouter = require('./routes/Customer-Router');
@@ -41,7 +42,59 @@ app.get('/', (req, res) => {
   });
 });
 
-http: app.use('/admin', AdminRouter);
+async function getAllAirports(req) {
+  let allAirports = [];
+  console.log(req.query.q);
+  let nextPage = `https://dir.aviapages.com/api/airports/?search_city=${req.query.q}`;
+
+  while (nextPage) {
+    try {
+      const response = await axios.get(nextPage, {
+        headers: {
+          accept: 'application/json',
+          Authorization: process.env.AVID_API_TOKEN,
+        },
+      });
+
+      if (response.status === 200) {
+        const pageData = response.data.results;
+        console.log('resultairports', pageData);
+        allAirports = allAirports.concat(pageData);
+        nextPage = response.data.next;
+      } else {
+        console.error('Failed to fetch aircraft data');
+        break;
+      }
+    } catch (error) {
+      console.error(error);
+      break;
+    }
+  }
+
+  return allAirports;
+}
+
+app.get('/all-airports', async (req, res) => {
+  try {
+    const airport = await getAllAirports(req);
+    console.log('airport', airport);
+    res.json(
+      // airport.map((airport) => ({
+      //   // airport_id: airport.airport_id,
+      //   // country_name: airport.country_name,
+      //   // icao: airport.icao,
+      //   // city_name: airport.city_name,
+      // }))
+      airport
+    );
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ error: 'Error fetching aircraft data' });
+  }
+});
+
+app.use('/admin', AdminRouter);
 app.use('/operator', OperatorRouter);
 app.use('/customer', CustomerRouter);
 app.use('/equiry', EquiryRouter);
